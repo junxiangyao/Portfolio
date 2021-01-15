70197b617f4e6c897aef9fe10811edb4c779c875
Junxiang Yao <junxiangyao@umail.ucsb.edu>
2021-01-15 11:06:56 -0800

    Side Menu; Hero Section; CoffeeChat
    Side Menu added in CoffeeChat, with addition in casestudy.css and scripts.css;
    CoffeeChat images up-to-date;
    Changed Hero sections with cover imges (for CoffeeChat and Cookwith)

--- newsite_jason_yao/js/scripts.js
+++ newsite_jason_yao/js/scripts.js
@@ -18,3 +18,51 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+let mainNavLinks = document.querySelectorAll("#side ul li a");
+let side = document.querySelectorAll("#side");
+let mainSections = document.querySelectorAll("main section");
+// document.body.scrollHeight
+let width=window.innerWidth;
+let lastId;
+let cur = [];
+
+// This should probably be throttled.
+// Especially because it triggers during smooth scrolling.
+// https://lodash.com/docs/4.17.10#throttle
+// You could do like...
+// window.addEventListener("scroll", () => {
+//    _.throttle(doThatStuff, 100);
+// });
+// Only not doing it here to keep this Pen dependency-free.
+
+window.addEventListener("scroll", event => {
+  let fromTop = window.scrollY;
+  // console.log(width);
+  if (fromTop < 600){
+    side[0].style.display = "none";
+  }else{
+    side[0].style.display = "block";
+    if(fromTop < 640){
+      side[0].classList.remove("show");
+    }else{
+      side[0].classList.add("show");
+    }
+    // if(width>=1024){
+    // }
+  }
+
+  mainNavLinks.forEach(link => {
+    let section = document.querySelector(link.hash);
+
+    if (
+      section.offsetTop - 10 <= fromTop &&
+      section.offsetTop + section.offsetHeight - 70 > fromTop
+    ) {
+      link.classList.add("current");
+    } else {
+      link.classList.remove("current");
+    }
+  });
+
+});
